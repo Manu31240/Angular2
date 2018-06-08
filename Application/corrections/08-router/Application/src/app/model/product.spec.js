14d83db95f35f8a19f35676e54485fd6cd8311f9
Gavalda Emmanuel <emmanuel.gavalda@orange.com>
2018-06-08 14:45:16 +0200

    add correction TP8

--- Application/corrections/08-router/Application/src/app/model/product.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/* tslint:disable:no-unused-variable */
-Object.defineProperty(exports, "__esModule", { value: true });
-var product_1 = require("./product");
-describe('Product', function () {
-    it('should create an instance', function () {
-        expect(new product_1.Product()).toBeTruthy();
-    });
-});
